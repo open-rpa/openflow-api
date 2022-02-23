f4dca1976eb4bf8ae6e080bda05963baacc1d6eb
Allan Zimmermann <allan@openrpa.dk>
2022-02-23 09:07:28 +0100

    add addqueue to RegisterExchangeMessage

--- src/Message/RegisterExchangeMessage.ts
+++ src/Message/RegisterExchangeMessage.ts
@@ -6,6 +6,7 @@ export class RegisterExchangeMessage {
     public algorithm: "direct" | "fanout" | "topic" | "header";
     public routingkey: string = "";
     public queuename: string;
+    public addqueue: string;
     static assign(o: any): RegisterExchangeMessage {
         if (typeof o === 'string' || o instanceof String) {
             return Object.assign(new RegisterExchangeMessage(), JSON.parse(o.toString()));
