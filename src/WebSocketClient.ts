b3ac7f020cac03608e319bef61f00b4725852323
Allan Zimmermann <allan@openrpa.dk>
2022-04-17 19:49:15 +0200

    cleanup websocketclient

--- src/WebSocketClient.ts
+++ src/WebSocketClient.ts
@@ -8,58 +8,6 @@ import { ApiConfig } from './ApiConfig';
 interface IHashTable<T> {
   [key: string]: T;
 }
-/** Provides the API for creating and managing a WebSocket connection to a server, as well as for sending and receiving data on the connection. */
-interface WebSocket extends EventTarget {
-  terminate(): void;
-  /**
-   * Returns a string that indicates how binary data from the WebSocket object is exposed to scripts:
-   * 
-   * Can be set, to change how binary data is returned. The default is "blob".
-   */
-  binaryType: BinaryType;
-  /**
-   * Returns the number of bytes of application data (UTF-8 text and binary data) that have been queued using send() but not yet been transmitted to the network.
-   * 
-   * If the WebSocket connection is closed, this attribute's value will only increase with each call to the send() method. (The number does not reset to zero once the connection closes.)
-   */
-  readonly bufferedAmount: number;
-  /**
-   * Returns the extensions selected by the server, if any.
-   */
-  readonly extensions: string;
-  onclose: ((this: WebSocket, ev: CloseEvent) => any) | null;
-  onerror: ((this: WebSocket, ev: Event) => any) | null;
-  onmessage: ((this: WebSocket, ev: MessageEvent) => any) | null;
-  onopen: ((this: WebSocket, ev: Event) => any) | null;
-  /**
-   * Returns the subprotocol selected by the server, if any. It can be used in conjunction with the array form of the constructor's second argument to perform subprotocol negotiation.
-   */
-  readonly protocol: string;
-  /**
-   * Returns the state of the WebSocket object's connection. It can have the values described below.
-   */
-  readonly readyState: number;
-  /**
-   * Returns the URL that was used to establish the WebSocket connection.
-   */
-  readonly url: string;
-  /**
-   * Closes the WebSocket connection, optionally using code as the the WebSocket connection close code and reason as the the WebSocket connection close reason.
-   */
-  close(code?: number, reason?: string): void;
-  /**
-   * Transmits data using the WebSocket connection. data can be a string, a Blob, an ArrayBuffer, or an ArrayBufferView.
-   */
-  send(data: string | ArrayBufferLike | Blob | ArrayBufferView): void;
-  readonly CLOSED: number;
-  readonly CLOSING: number;
-  readonly CONNECTING: number;
-  readonly OPEN: number;
-  addEventListener<K extends keyof WebSocketEventMap>(type: K, listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
-  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
-  removeEventListener<K extends keyof WebSocketEventMap>(type: K, listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
-  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
-}
 
 declare const WebSocket: {
   prototype: WebSocket;
@@ -184,7 +132,9 @@ export class WebSocketClient {
         if (ApiConfig.log_error) this._logger.error(error);
       }
       try {
-        this._socketObject.terminate();
+        if ((this._socketObject as any).terminate) {
+          (this._socketObject as any).terminate();
+        }        
       } catch (error) {
         if (ApiConfig.log_error) this._logger.error(error);
       }
@@ -429,7 +379,7 @@ export class WebSocketClient {
         for (let i = this._sendQueue.length - 1; i >= 0; i--) {
           try {
             const msg = this._sendQueue[i];
-            if (msg.command !== "unwatch" && msg.command !== "watch") {
+            if (msg.command !== "unwatch" && msg.command !== "watch" && msg.command !== "signin") {
               this.messageStore.set(this.messageCounter, JSON.stringify(msg));
             }
             this._sendQueue.splice(i, 1);
